e8306be3d0d42fd0da8e360e8753446f6b88c86b
matkocal <jyfmpm8562@privaterelay.appleid.com>
2026-04-01 21:14:33 +0200

    Update scientificcalc.c

--- scientificcalc.c
+++ scientificcalc.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 
+// Logarithm calculator
 void log_calc() {
     int choice;
     double num;
 
-    printf("----- LOG CALCULATOR -----\n");
+    printf("\n----- LOG CALCULATOR -----\n");
     printf("1. log10(x)\n");
     printf("2. ln(x)\n");
     printf("Choose an option: ");
-    scanf("%d", &choice);
+
+    if (scanf("%d", &choice) != 1) {
+        printf("Invalid input\n");
+        return;
+    }
 
     printf("Enter a number: ");
-    scanf("%lf", &num);
+    if (scanf("%lf", &num) != 1) {
+        printf("Invalid input\n");
+        return;
+    }
 
     if (num <= 0) {
         printf("Error: log undefined for non-positive numbers\n");
         return;
     }
 
-       switch (choice) {
+    switch (choice) {
         case 1:
             printf("log10(%.2f) = %.4f\n", num, log10(num));
             break;
@@ -31,62 +39,75 @@ void log_calc() {
     }
 }
 
-
-
-
-
+// Basic calculator
 int calc() {
-char operator;
-double num1, num2;
+    char operator;
+    double num1, num2;
 
+    printf("\n----- BASIC CALCULATOR -----\n");
+    printf("Enter an operator (+, -, *, /): ");
 
+    if (scanf(" %c", &operator) != 1) {
+        printf("Invalid input\n");
+        return 1;
+    }
 
+    printf("Enter two numbers: ");
+    if (scanf("%lf %lf", &num1, &num2) != 2) {
+        printf("Invalid input\n");
+        return 1;
+    }
 
-printf("enter an operator (+, -, *, /): \n");
-scanf(" %c", &operator);
+    switch (operator) {
+        case '+':
+            printf("Result: %.2f\n", num1 + num2);
+            break;
+        case '-':
+            printf("Result: %.2f\n", num1 - num2);
+            break;
+        case '*':
+            printf("Result: %.2f\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 == 0) {
+                printf("Error: division by zero\n");
+            } else {
+                printf("Result: %.2f\n", num1 / num2);
+            }
+            break;
+        default:
+            printf("Error: invalid operator\n");
+    }
 
-   printf("Enter two numbers: ");
-    scanf("%lf %lf", &num1, &num2);
-
-switch (operator) {
-    case '+':
-        printf("result: %.2f\n", num1 + num2);
-        break;
-    case '-':
-        printf("result: %.2f\n", num1 - num2);
-        break;
-    case '*':
-        printf("result: %.2f\n", num1 * num2);
-        break;
-    case '/':
-        printf("result: %.2f\n", num1 / num2);
-        break;
-    default:
-        printf("error: invalid operator\n");
-}
     return 0;
 }
 
 int main() {
     int choice;
 
+    while (1) {
+        printf("\n========= SCIENTIFIC CALCULATOR =========\n");
+        printf("1. Basic Calculator\n");
+        printf("2. Log Calculator\n");
+        printf("0. Exit\n");
+        printf("Choose: ");
 
-   printf("---------SCIENTIFIC CALCULATOR---------\n");
-    printf("1. Basic Calculator\n");
-    printf("2. Log Calculator\n");
-    printf("Choose: ");
-    scanf("%d", &choice);
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid input\n");
+            return 1;
+        }
 
-    if (choice == 1) {
-        calc();
-    } else if (choice == 2) {
-        log_calc();
-    } else {
-        printf("Invalid choice\n");
+        if (choice == 1) {
+            calc();
+        } else if (choice == 2) {
+            log_calc();
+        } else if (choice == 0) {
+            printf("Goodbye!\n");
+            break;
+        } else {
+            printf("Invalid choice\n");
+        }
     }
 
     return 0;
 }
-
-
-
