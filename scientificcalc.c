bc6c16db4b0bd6d5bd6157298135c61a2296548c
matkocal <jyfmpm8562@privaterelay.appleid.com>
2026-04-01 21:13:00 +0200

    Implement log calculator functionality
    Added a log calculator function to handle log10 and natural log calculations based on user choice.

--- scientificcalc.c
+++ scientificcalc.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
+#include <math.h>
+
+void log_calc() {
+    int choice;
+    double num;
+
+    printf("----- LOG CALCULATOR -----\n");
+    printf("1. log10(x)\n");
+    printf("2. ln(x)\n");
+    printf("Choose an option: ");
+    scanf("%d", &choice);
+
+    printf("Enter a number: ");
+    scanf("%lf", &num);
+
+    if (num <= 0) {
+        printf("Error: log undefined for non-positive numbers\n");
+        return;
+    }
+
+       switch (choice) {
+        case 1:
+            printf("log10(%.2f) = %.4f\n", num, log10(num));
+            break;
+        case 2:
+            printf("ln(%.2f) = %.4f\n", num, log(num));
+            break;
+        default:
+            printf("Invalid choice\n");
+    }
+}
+
+
+
+
 
 int calc() {
 char operator;
 double num1, num2;
 
-printf("---------SCIENTIFIC CALCULATOR---------\n");
+
 
 
 printf("enter an operator (+, -, *, /): \n");
@@ -33,7 +68,25 @@ switch (operator) {
 }
 
 int main() {
-    calc();
-    return 0;
+    int choice;
 
+
+   printf("---------SCIENTIFIC CALCULATOR---------\n");
+    printf("1. Basic Calculator\n");
+    printf("2. Log Calculator\n");
+    printf("Choose: ");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        calc();
+    } else if (choice == 2) {
+        log_calc();
+    } else {
+        printf("Invalid choice\n");
+    }
+
+    return 0;
 }
+
+
+
